9162aca7d71c0bcf8d736354af0e45a767dab039
Tasvir Limbani <120560952+TasvirLimbani@users.noreply.github.com>
2024-06-10 10:46:39 +0530

    fine

--- src/components/Navbara.js
+++ src/components/Navbara.js
@@ -23,6 +23,11 @@ import FadingGallery from './FadingGallery';
 
 const Navbar = () => {
 
+    const images = [
+        { id: 1, src: offer, alt: 'Image 1' },
+        { id: 2, src: offer02, alt: 'Image 2' },
+        { id: 3, src: offer03, alt: 'Image 3' },
+    ];
 
 
     return (
@@ -91,71 +96,216 @@ const Navbar = () => {
 
             </div>
 
-                {/* contecdt */}
+            <div className="top-destinations">
+                <h2>Top destinations</h2>
 
-            <div className='container mt-5'>
+                <div class="container-a4">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6  mt-4">
+                            <div class="caption-style-4">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/103389/chaps_1x.jpg" alt="" class="img-fluid" />
+                                <div class="caption">
+                                    <div class="blur"></div>
+                                    <div class="caption-text">
+                                        <h1>Amazing Caption</h1>
+                                        <p>Whatever It Is - Always Awesome</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 mt-4">
+                            <div class="caption-style-4">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/103389/chaps_1x.jpg" alt="" class="img-fluid" />
+                                <div class="caption">
+                                    <div class="blur"></div>
+                                    <div class="caption-text">
+                                        <h1>Amazing Caption</h1>
+                                        <p>Whatever It Is - Always Awesome</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 mt-4">
+                            <div class="caption-style-4">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/103389/chaps_1x.jpg" alt="" class="img-fluid" />
+                                <div class="caption">
+                                    <div class="blur"></div>
+                                    <div class="caption-text">
+                                        <h1>Amazing Caption</h1>
+                                        <p>Whatever It Is - Always Awesome</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-
-                <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-                    <MDBCol>
-                        <MDBCard className='h-100'>
-                            <MDBCardImage
-                                src='https://mdbootstrap.com/img/new/standard/city/044.webp'
-                                alt='...'
-                                position='top'
-                            />
-                            <MDBCardBody>
-                                <MDBCardTitle>Card title</MDBCardTitle>
-                                <MDBCardText>
-                                    This is a longer card with supporting text below as a natural lead-in to additional content.
-                                    This content is a little bit longer.
-                                </MDBCardText>
-                            </MDBCardBody>
-                            <MDBCardFooter>
-                                <small className='text-muted'>Last updated 3 mins ago</small>
-                            </MDBCardFooter>
-                        </MDBCard>
-                    </MDBCol>
-                    <MDBCol>
-                        <MDBCard className='h-100'>
-                            <MDBCardImage
-                                src='https://mdbootstrap.com/img/new/standard/city/043.webp'
-                                alt='...'
-                                position='top'
-                            />
-                            <MDBCardBody>
-                                <MDBCardTitle>Card title</MDBCardTitle>
-                                <MDBCardText>
-                                    This card has supporting text below as a natural lead-in to additional content.
-                                </MDBCardText>
-                            </MDBCardBody>
-                            <MDBCardFooter>
-                                <small className='text-muted'>Last updated 3 mins ago</small>
-                            </MDBCardFooter>
-                        </MDBCard>
-                    </MDBCol>
-                    <MDBCol>
-                        <MDBCard className='h-100'>
-                            <MDBCardImage
-                                src='https://mdbootstrap.com/img/new/standard/city/042.webp'
-                                alt='...'
-                                position='top'
-                            />
-                            <MDBCardBody>
-                                <MDBCardTitle>Card title</MDBCardTitle>
-                                <MDBCardText>
-                                    This is a wider card with supporting text below as a natural lead-in to additional content. This
-                                    card has even longer content than the first to show that equal height action.
-                                </MDBCardText>
-                            </MDBCardBody>
-                            <MDBCardFooter>
-                                <small className='text-muted'>Last updated 3 mins ago</small>
-                            </MDBCardFooter>
-                        </MDBCard>
-                    </MDBCol>
-                </MDBRow>
+                        <div class="col-md-4 col-sm-6 mt-4">
+                            <div class="caption-style-4">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/103389/chaps_1x.jpg" alt="" class="img-fluid" />
+                                <div class="caption">
+                                    <div class="blur"></div>
+                                    <div class="caption-text">
+                                        <h1>Amazing Caption</h1>
+                                        <p>Whatever It Is - Always Awesome</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 mt-4">
+                            <div class="caption-style-4">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/103389/everycowboy_dribbbleready_shot.jpg" alt="" class="img-fluid" />
+                                <div class="caption">
+                                    <div class="blur"></div>
+                                    <div class="caption-text">
+                                        <h1>Amazing Caption</h1>
+                                        <p>Whatever It Is - Always Awesome</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 mt-4">
+                            <div class="caption-style-4">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/103389/ithinkican_01.jpg" alt="" class="img-fluid" />
+                                <div class="caption">
+                                    <div class="blur"></div>
+                                    <div class="caption-text">
+                                        <h1>Amazing Caption</h1>
+                                        <p>Whatever It Is - Always Awesome</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- Repeat similar structure for other images --> */}
+                    </div>
+                </div>
             </div>
-            
+
+            {/* contecdt */}
+
+            <div className='rem-1 '>
+                <div className='container mt-5'>
+
+
+                    <h1 style={{ textAlign: "center", marginTop: "50px", padding: '43px' }}>Recommended for you</h1>
+
+                    <MDBRow className='row-cols-1 row-cols-md-3 mt-4 g-4'>
+                        <MDBCol>
+                            <MDBCard className='h-100'>
+                                <MDBCardImage
+                                    src='https://mdbootstrap.com/img/new/standard/city/044.webp'
+                                    alt='...'
+                                    position='top'
+                                />
+                                <MDBCardBody>
+                                    <MDBCardTitle>Card title</MDBCardTitle>
+                                    <MDBCardText>
+                                        This is a longer card with supporting text below as a natural lead-in to additional content.
+                                        This content is a little bit longer.
+                                    </MDBCardText>
+                                </MDBCardBody>
+                                <MDBCardFooter>
+                                    <small className='text-muted'>Last updated 3 mins ago</small>
+                                </MDBCardFooter>
+                            </MDBCard>
+                        </MDBCol>
+                        <MDBCol>
+                            <MDBCard className='h-100'>
+                                <MDBCardImage
+                                    src='https://mdbootstrap.com/img/new/standard/city/043.webp'
+                                    alt='...'
+                                    position='top'
+                                />
+                                <MDBCardBody>
+                                    <MDBCardTitle>Card title</MDBCardTitle>
+                                    <MDBCardText>
+                                        This card has supporting text below as a natural lead-in to additional content.
+                                    </MDBCardText>
+                                </MDBCardBody>
+                                <MDBCardFooter>
+                                    <small className='text-muted'>Last updated 3 mins ago</small>
+                                </MDBCardFooter>
+                            </MDBCard>
+                        </MDBCol>
+                        <MDBCol>
+                            <MDBCard className='h-100'>
+                                <MDBCardImage
+                                    src='https://mdbootstrap.com/img/new/standard/city/042.webp'
+                                    alt='...'
+                                    position='top'
+                                />
+                                <MDBCardBody>
+                                    <MDBCardTitle>Card title</MDBCardTitle>
+                                    <MDBCardText>
+                                        This is a wider card with supporting text below as a natural lead-in to additional content. This
+                                        card has even longer content than the first to show that equal height action.
+                                    </MDBCardText>
+                                </MDBCardBody>
+                                <MDBCardFooter>
+                                    <small className='text-muted'>Last updated 3 mins ago</small>
+                                </MDBCardFooter>
+                            </MDBCard>
+                        </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow className='row-cols-1 row-cols-md-3 mt-4 g-4'>
+                        <MDBCol>
+                            <MDBCard className='h-100'>
+                                <MDBCardImage
+                                    src='https://mdbootstrap.com/img/new/standard/city/044.webp'
+                                    alt='...'
+                                    position='top'
+                                />
+                                <MDBCardBody>
+                                    <MDBCardTitle>Card title</MDBCardTitle>
+                                    <MDBCardText>
+                                        This is a longer card with supporting text below as a natural lead-in to additional content.
+                                        This content is a little bit longer.
+                                    </MDBCardText>
+                                </MDBCardBody>
+                                <MDBCardFooter>
+                                    <small className='text-muted'>Last updated 3 mins ago</small>
+                                </MDBCardFooter>
+                            </MDBCard>
+                        </MDBCol>
+                        <MDBCol>
+                            <MDBCard className='h-100'>
+                                <MDBCardImage
+                                    src='https://mdbootstrap.com/img/new/standard/city/043.webp'
+                                    alt='...'
+                                    position='top'
+                                />
+                                <MDBCardBody>
+                                    <MDBCardTitle>Card title</MDBCardTitle>
+                                    <MDBCardText>
+                                        This card has supporting text below as a natural lead-in to additional content.
+                                    </MDBCardText>
+                                </MDBCardBody>
+                                <MDBCardFooter>
+                                    <small className='text-muted'>Last updated 3 mins ago</small>
+                                </MDBCardFooter>
+                            </MDBCard>
+                        </MDBCol>
+                        <MDBCol>
+                            <MDBCard className='h-100'>
+                                <MDBCardImage
+                                    src='https://mdbootstrap.com/img/new/standard/city/042.webp'
+                                    alt='...'
+                                    position='top'
+                                />
+                                <MDBCardBody>
+                                    <MDBCardTitle>Card title</MDBCardTitle>
+                                    <MDBCardText>
+                                        This is a wider card with supporting text below as a natural lead-in to additional content. This
+                                        card has even longer content than the first to show that equal height action.
+                                    </MDBCardText>
+                                </MDBCardBody>
+                                <MDBCardFooter>
+                                    <small className='text-muted'>Last updated 3 mins ago</small>
+                                </MDBCardFooter>
+                            </MDBCard>
+                        </MDBCol>
+                    </MDBRow>
+                </div>
+            </div>
             <FadingGallery />
 
         </>
